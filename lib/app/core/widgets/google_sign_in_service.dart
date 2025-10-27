f693e9cd75d755e52a73f0c3583dd13ae371bf8e
Gopal <info@constructionmartai.com>
2025-10-27 16:45:19 +0530

    android google login complete

--- lib/app/core/widgets/google_sign_in_service.dart
+++ lib/app/core/widgets/google_sign_in_service.dart
@@ -7,18 +7,50 @@ import 'package:google_sign_in/google_sign_in.dart';
 class GoogleSignInService {
   static final FirebaseAuth _auth = FirebaseAuth.instance;
   static final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
+  static bool _isInitialized = false;
+
+  static Future<void> _initialize() async {
+    if (!_isInitialized) {
+      await _googleSignIn.initialize(
+        clientId:
+            '102792963565-3ugsov23978rn4s16ugstv4favjdmacp.apps.googleusercontent.com',
+        serverClientId:
+            '484988555302-d91nev5jn5sit0qoe3oehpgpp58pl5mt.apps.googleusercontent.com',
+      );
+      _isInitialized = true;
+    }
+  }
 
   static Future<User?> signInWithGoogle() async {
     try {
+      await _initialize();
+
+      await _googleSignIn.signOut();
+
       final GoogleSignInAccount googleUser = await _googleSignIn.authenticate();
 
       final GoogleSignInAuthentication googleAuth = googleUser.authentication;
 
-      final credential = GoogleAuthProvider.credential(idToken: googleAuth.idToken);
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(
+        credential,
+      );
 
       return userCredential.user;
+    } on GoogleSignInException catch (e) {
+      switch (e.code) {
+        case GoogleSignInExceptionCode.canceled:
+          log('User canceled the sign-in process');
+          return null;
+        default:
+          log('Google Sign-In error: ${e.code}');
+          rethrow;
+      }
     } catch (e) {
+      log('General Google Sign-In error: $e');
       rethrow;
     }
   }
