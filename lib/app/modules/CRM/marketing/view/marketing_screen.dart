c0491f7e7ff98b111ba2442e51694874809591fd
Vibhuti <109781642+VibhutiGondaliya@users.noreply.github.com>
2025-11-18 11:35:48 +0530

    --marketing header

--- lib/app/modules/CRM/marketing/view/marketing_screen.dart
+++ lib/app/modules/CRM/marketing/view/marketing_screen.dart
@@ -1,3 +1,4 @@
+import 'package:construction_technect/app/core/utils/common_appbar.dart';
 import 'package:construction_technect/app/core/utils/common_fun.dart';
 import 'package:construction_technect/app/core/utils/imports.dart';
 import 'package:construction_technect/app/modules/CRM/marketing/controller/marketing_controller.dart';
@@ -5,7 +6,6 @@ import 'package:construction_technect/app/modules/CRM/marketing/view/widget/add_
 import 'package:construction_technect/app/modules/CRM/marketing/view/widget/lead_item_Card.dart';
 import 'package:construction_technect/app/modules/CRM/marketing/view/widget/segment_filters_widget.dart';
 import 'package:construction_technect/app/modules/CRM/marketing/view/widget/todays_leads_card.dart';
-import 'package:construction_technect/app/modules/CRM/marketing/view/widget/topbar_header.dart';
 import 'package:construction_technect/app/modules/CRM/marketing/view/widget/white_card_widget.dart';
 
 class MarketingScreen extends GetView<MarketingController> {
@@ -28,68 +28,75 @@ class MarketingScreen extends GetView<MarketingController> {
                   ),
                 ),
               ),
-
-              const TopBarHeader(),
-
-              Positioned.fill(
-                top: 110,
-                child: SafeArea(
-                  top: false,
-                  child: SingleChildScrollView(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 20),
-                          const SegmentFiltersWidget(),
-                          const SizedBox(height: 20),
-
-                          WhiteCardWidget(
-                            child: Column(
-                              children: [
-                                AddNewLeadButton(
-                                  onTap: () => controller.onAdd(context),
-                                ),
-                                const SizedBox(height: 18),
-                                const TodaysLeadsCard(),
-                                const SizedBox(height: 20),
-
-                                Row(
-                                  children: [
-                                    Text(
-                                      'Lead Details',
-                                      style: MyTexts.medium20,
-                                    ),
-                                  ],
-                                ),
-
-                                const SizedBox(height: 12),
-
-                                Obx(
-                                  () => Column(
-                                    children: controller.leads
-                                        .map(
-                                          (l) => Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                              vertical: 10,
-                                            ),
-                                            child: LeadItemCard(
-                                              lead: l,
-                                              controller: controller,
-                                            ),
-                                          ),
-                                        )
-                                        .toList(),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
+              Column(
+                children: [
+                  CommonAppBar(
+                    backgroundColor: Colors.transparent,
+                    isCenter: false,
+                    title: const Text("Marketing"),
+                    leading: GestureDetector(
+                      onTap: Get.back,
+                      child: const Padding(
+                        padding: EdgeInsets.zero,
+                        child: Icon(Icons.arrow_back_ios_new_sharp, color: Colors.black, size: 20),
                       ),
                     ),
                   ),
-                ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Column(
+                          children: [
+                            const SegmentFiltersWidget(),
+                            const SizedBox(height: 20),
+                            WhiteCardWidget(
+                              child: Column(
+                                children: [
+                                  AddNewLeadButton(
+                                    onTap: () => controller.onAdd(context),
+                                  ),
+                                  const SizedBox(height: 18),
+                                  const TodaysLeadsCard(),
+                                  const SizedBox(height: 20),
+
+                                  Row(
+                                    children: [
+                                      Text(
+                                        'Lead Details',
+                                        style: MyTexts.medium20,
+                                      ),
+                                    ],
+                                  ),
+
+                                  const SizedBox(height: 12),
+
+                                  Obx(
+                                        () => Column(
+                                      children: controller.leads
+                                          .map(
+                                            (l) => Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            vertical: 10,
+                                          ),
+                                          child: LeadItemCard(
+                                            lead: l,
+                                            controller: controller,
+                                          ),
+                                        ),
+                                      )
+                                          .toList(),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
