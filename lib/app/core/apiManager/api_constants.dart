1b1ff578943f3f998c4667f17c082930f26971bb
Adhil ConstructionTechnect <adhil.constructiontechnect@gmail.com>
2025-12-05 15:44:49 +0530

    pos

--- lib/app/core/apiManager/api_constants.dart
+++ lib/app/core/apiManager/api_constants.dart
@@ -13,9 +13,6 @@ class APIConstants {
 
   static const String notificationToggle = "notification-settings/toggle";
 
-  static const String crmAnalytics = "crm/analytics";
-  static const String crmAnalyticsPdf = "crm/analytics/pdf";
-
   static const String deactivateSendOtp = "auth/deactivate/send-otp";
   static const String deactivateVerifyOtp = "auth/deactivate/verify-otp";
   static const String deleteSendOtp = "auth/delete/send-otp";
@@ -144,4 +141,6 @@ class APIConstants {
   static const String autoFill = "crm/lead/auto-fill";
   static const String crmLead = "crm/lead";
   static const String crmSalesLead = "crm/sales-lead";
+  static const String crmAnalytics = "crm/analytics";
+  static const String crmAnalyticsPdf = "crm/analytics/pdf";
 }
