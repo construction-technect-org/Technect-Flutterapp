6bda906abcdf34bb74d78cea41a6b1c97e49d194
Adhil ConstructionTechnect <adhil.constructiontechnect@gmail.com>
2025-12-05 12:51:28 +0530

    Crm Analysis Api added

--- lib/app/modules/CRM/more/analysis/controller/analysis_controller.dart
+++ lib/app/modules/CRM/more/analysis/controller/analysis_controller.dart
@@ -1,13 +1,18 @@
-import 'dart:io';
-
 import 'package:construction_technect/app/core/utils/imports.dart';
 import 'package:construction_technect/app/modules/MarketPlace/Partner/Home/QuickAccess/Report/model/analysis_model.dart';
 import 'package:construction_technect/app/modules/MarketPlace/Partner/Home/QuickAccess/Report/services/AnalysisService.dart';
-import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
 
 class AnalysisController extends GetxController {
   // final statusCards = <Map<String, dynamic>>[].obs;
+  @override
+  void onInit() {
+    super.onInit();
+    if (Get.arguments != null) {
+      isReport.value = Get.arguments["isReport"];
+    }
+    fetchAnalysisByDD();
+  }
+
   RxBool isLoading = false.obs;
   RxBool isReport = false.obs;
 
@@ -15,7 +20,7 @@ class AnalysisController extends GetxController {
 
   Rx<DateTime?> startMonth = Rx<DateTime?>(null);
   Rx<DateTime?> endMonth = Rx<DateTime?>(null);
-  final RxString selectedPeriod = "Last 6 Months".obs;
+  final RxString selectedPeriod = "Last 3 Months".obs;
   final List<String> periodOptions = [
     "Last 3 Months",
     "Last 6 Months",
@@ -51,7 +56,7 @@ class AnalysisController extends GetxController {
 
     try {
       isLoading.value = true;
-      final result = await AnalysisService().fetchAllAnalysis(
+      final result = await AnalysisService().fetchAllCRMAnalysis(
         startMonth: startMonth.value!.month.toString(),
         startYear: startMonth.value!.year.toString(),
         endMonth: endMonth.value!.month.toString(),
@@ -79,7 +84,7 @@ class AnalysisController extends GetxController {
   Future<void> fetchAnalysisByDD() async {
     try {
       isLoading.value = true;
-      final result = await AnalysisService().fetchAllAnalysisByPeriod(
+      final result = await AnalysisService().fetchAllCRMAnalysisByPeriod(
         period: selectedPeriod.value.toLowerCase(),
       );
       if (result != null && result.success == true) {
@@ -93,94 +98,94 @@ class AnalysisController extends GetxController {
   }
 
   Future<void> downloadReportPdf({required bool isPeriod}) async {
-    try {
-      isLoading.value = true;
+    //   try {
+    //     isLoading.value = true;
 
-      final pdfBytes = await AnalysisService().fetchReportPdfByPeriod(
-        isPeriod: isPeriod,
-        period: isPeriod ? selectedPeriod.value.toLowerCase() : "",
-        startMonth: !isPeriod ? startMonth.value!.month.toString() : "",
-        startYear: !isPeriod ? startMonth.value!.year.toString() : "",
-        endMonth: !isPeriod ? endMonth.value!.month.toString() : "",
-        endYear: !isPeriod ? endMonth.value!.year.toString() : "",
-        token: myPref.getToken(),
-      );
+    //     final pdfBytes = await AnalysisService().fetchReportPdfByPeriod(
+    //       isPeriod: isPeriod,
+    //       period: isPeriod ? selectedPeriod.value.toLowerCase() : "",
+    //       startMonth: !isPeriod ? startMonth.value!.month.toString() : "",
+    //       startYear: !isPeriod ? startMonth.value!.year.toString() : "",
+    //       endMonth: !isPeriod ? endMonth.value!.month.toString() : "",
+    //       endYear: !isPeriod ? endMonth.value!.year.toString() : "",
+    //       token: myPref.getToken(),
+    //     );
 
-      if (pdfBytes != null) {
-        Directory dir;
+    //     if (pdfBytes != null) {
+    //       Directory dir;
 
-        if (Platform.isAndroid) {
-          dir = Directory('/storage/emulated/0/Download');
-          if (!await dir.exists()) {
-            dir = await getApplicationDocumentsDirectory();
-          }
-        } else {
-          dir = await getApplicationDocumentsDirectory();
-        }
+    //       if (Platform.isAndroid) {
+    //         dir = Directory('/storage/emulated/0/Download');
+    //         if (!await dir.exists()) {
+    //           dir = await getApplicationDocumentsDirectory();
+    //         }
+    //       } else {
+    //         dir = await getApplicationDocumentsDirectory();
+    //       }
 
-        final String filename =
-            '${dir.path}/report_${selectedPeriod.value.toLowerCase()}${DateTime.now().microsecondsSinceEpoch}.pdf'
-                .replaceAll(" ", "");
-        final file = File(filename);
-        await file.writeAsBytes(pdfBytes);
+    //       final String filename =
+    //           '${dir.path}/report_${selectedPeriod.value.toLowerCase()}${DateTime.now().microsecondsSinceEpoch}.pdf'
+    //               .replaceAll(" ", "");
+    //       final file = File(filename);
+    //       await file.writeAsBytes(pdfBytes);
 
-        Get.defaultDialog(
-          titlePadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          backgroundColor: Colors.white,
-          title: "Success",
-          titleStyle: MyTexts.medium16.copyWith(color: MyColors.gray2E),
-          middleTextStyle: MyTexts.medium14.copyWith(color: MyColors.gray54),
-          middleText: "PDF downloaded successfully",
-          actions: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Flexible(
-                  child: RoundedButton(
-                    onTap: () => Get.back(),
-                    buttonName: 'Cancel',
-                    borderRadius: 12,
-                    verticalPadding: 0,
-                    height: 45,
-                    color: MyColors.grayCD,
-                  ),
-                ),
-                const SizedBox(width: 8),
-                Flexible(
-                  child: RoundedButton(
-                    onTap: () {
-                      OpenFilex.open(file.path);
-                      Get.back();
-                    },
-                    buttonName: "Open",
-                    borderRadius: 12,
-                    verticalPadding: 0,
-                    height: 45,
-                    color: MyColors.primary,
-                  ),
-                ),
-              ],
-            ),
-          ],
-        );
-      } else {
-        Get.defaultDialog(
-          title: "Error",
-          middleText: "Failed to download PDF",
-          textConfirm: "OK",
-          onConfirm: () => Get.back(),
-        );
-      }
-    } catch (e) {
-      if (kDebugMode) print("Error downloading PDF: $e");
-      Get.defaultDialog(
-        title: "Error",
-        middleText: "Something went wrong",
-        textConfirm: "OK",
-        onConfirm: () => Get.back(),
-      );
-    } finally {
-      isLoading.value = false;
-    }
+    //       Get.defaultDialog(
+    //         titlePadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+    //         backgroundColor: Colors.white,
+    //         title: "Success",
+    //         titleStyle: MyTexts.medium16.copyWith(color: MyColors.gray2E),
+    //         middleTextStyle: MyTexts.medium14.copyWith(color: MyColors.gray54),
+    //         middleText: "PDF downloaded successfully",
+    //         actions: [
+    //           Row(
+    //             mainAxisSize: MainAxisSize.min,
+    //             children: [
+    //               Flexible(
+    //                 child: RoundedButton(
+    //                   onTap: () => Get.back(),
+    //                   buttonName: 'Cancel',
+    //                   borderRadius: 12,
+    //                   verticalPadding: 0,
+    //                   height: 45,
+    //                   color: MyColors.grayCD,
+    //                 ),
+    //               ),
+    //               const SizedBox(width: 8),
+    //               Flexible(
+    //                 child: RoundedButton(
+    //                   onTap: () {
+    //                     OpenFilex.open(file.path);
+    //                     Get.back();
+    //                   },
+    //                   buttonName: "Open",
+    //                   borderRadius: 12,
+    //                   verticalPadding: 0,
+    //                   height: 45,
+    //                   color: MyColors.primary,
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //         ],
+    //       );
+    //     } else {
+    //       Get.defaultDialog(
+    //         title: "Error",
+    //         middleText: "Failed to download PDF",
+    //         textConfirm: "OK",
+    //         onConfirm: () => Get.back(),
+    //       );
+    //     }
+    //   } catch (e) {
+    //     if (kDebugMode) print("Error downloading PDF: $e");
+    //     Get.defaultDialog(
+    //       title: "Error",
+    //       middleText: "Something went wrong",
+    //       textConfirm: "OK",
+    //       onConfirm: () => Get.back(),
+    //     );
+    //   } finally {
+    //     isLoading.value = false;
+    //   }
   }
 }
--- lib/app/modules/CRM/more/analysis/widgets/report_chart.dart
+++ lib/app/modules/CRM/more/analysis/widgets/report_chart.dart
@@ -13,238 +13,66 @@ class ReportChart extends GetView<AnalysisController> {
     return Obx(() {
       if (!controller.isReport.value) {
         final analysis = controller.analysisModel.value;
-        return myPref.role.val == "partner"
-            ? Column(
-                children: [
-                  if (analysis.productAnalytics != null)
-                    ReportGraph(
-                      title: "Products Analysis",
-                      labels: analysis.productAnalytics!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.productAnalytics!.monthlyBreakdown!
-                            .map((e) => e.productsAdded ?? 0)
-                            .toList(),
-                        analysis.productAnalytics!.monthlyBreakdown!
-                            .map((e) => e.activeProducts ?? 0)
-                            .toList(),
-                        analysis.productAnalytics!.monthlyBreakdown!
-                            .map((e) => e.rejectedProducts ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, MyColors.green, Colors.red],
-                      legends: const ["Products Added", "Active Products", "Rejected Products"],
-                    ),
-                  const Gap(10),
-                  if (analysis.serviceAnalytics != null)
-                    ReportGraph(
-                      title: "Services Analysis",
-                      labels: analysis.serviceAnalytics!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.serviceAnalytics!.monthlyBreakdown!
-                            .map((e) => e.servicesAdded ?? 0)
-                            .toList(),
-                        analysis.serviceAnalytics!.monthlyBreakdown!
-                            .map((e) => e.activeServices ?? 0)
-                            .toList(),
-                        analysis.serviceAnalytics!.monthlyBreakdown!
-                            .map((e) => e.rejectedServices ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, MyColors.green, Colors.red],
-                      legends: const ["Services Added", "Active Services", "Rejected Services"],
-                    ),
-                  const Gap(10),
-                  if (analysis.teamAnalytics != null)
-                    ReportGraph(
-                      title: "Team Analysis",
-                      labels: analysis.teamAnalytics!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.teamAnalytics!.monthlyBreakdown!
-                            .map((e) => e.teamMembersAdded ?? 0)
-                            .toList(),
-                        analysis.teamAnalytics!.monthlyBreakdown!
-                            .map((e) => e.availableTeamMembers ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, MyColors.green],
-                      legends: const ["Added Member", "Active Member"],
-                    ),
-                  const Gap(10),
-
-                  if (analysis.roleAnalytics != null)
-                    ReportGraph(
-                      title: "Role Analysis",
-                      labels: analysis.roleAnalytics!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.roleAnalytics!.monthlyBreakdown!
-                            .map((e) => e.rolesCreated ?? 0)
-                            .toList(),
-                        analysis.roleAnalytics!.monthlyBreakdown!
-                            .map((e) => e.activeRoles ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, MyColors.green],
-                      legends: const ["Role Created", "Active Role"],
-                    ),
-                  const Gap(10),
-                  if (analysis.supportTicketAnalytics != null)
-                    ReportGraph(
-                      title: "Support Ticket Analysis",
-                      labels: analysis.supportTicketAnalytics!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.supportTicketAnalytics!.monthlyBreakdown!
-                            .map((e) => e.openTickets ?? 0)
-                            .toList(),
-                        analysis.supportTicketAnalytics!.monthlyBreakdown!
-                            .map((e) => e.closedTickets ?? 0)
-                            .toList(),
-                        analysis.supportTicketAnalytics!.monthlyBreakdown!
-                            .map((e) => e.resolvedTickets ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [Colors.orange, Colors.red, MyColors.green],
-                      legends: const ["Open Tickets", "Closed Tickets", "Resoled Tickets"],
-                    ),
-                  const Gap(20),
+        return Column(
+          children: [
+            if (analysis.leadAnalytics != null && analysis.leadAnalytics!.monthlyBreakdown != null)
+              ReportGraph(
+                title: "Lead Analysis",
+                labels: analysis.leadAnalytics!.monthlyBreakdown!.map((e) => e.monthName!).toList(),
+                values: [
+                  analysis.leadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.leadsCreated ?? 0)
+                      .toList(),
+                  analysis.leadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.convertedToSales ?? 0)
+                      .toList(),
                 ],
-              )
-            : Column(
-                children: [
-                  if (analysis.wishlist != null)
-                    ReportGraph(
-                      title: "WishList Analysis",
-                      labels: analysis.wishlist!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.wishlist!.monthlyBreakdown!.map((e) => e.itemsAdded ?? 0).toList(),
-                      ],
-                      colors: const [MyColors.primary, MyColors.green, Colors.red],
-                      legends: const ["Item Added"],
-                    ),
-                  const Gap(10),
-                  if (analysis.productConnections != null)
-                    ReportGraph(
-                      title: "Product Connections Analysis",
-                      labels: analysis.productConnections!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.productConnections!.monthlyBreakdown!
-                            .map((e) => e.connectionsCreated ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary],
-                      legends: const ["Product Connections Created"],
-                    ),
-                  const Gap(10),
-                  if (analysis.serviceConnections != null)
-                    ReportGraph(
-                      title: "Service Connections Analysis",
-                      labels: analysis.serviceConnections!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.serviceConnections!.monthlyBreakdown!
-                            .map((e) => e.serviceConnectionsCreated ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.green],
-                      legends: const ["Service Connections Created"],
-                    ),
-                  const Gap(10),
-                  if (analysis.serviceRequirements != null)
-                    ReportGraph(
-                      title: "Service Requirements Analysis",
-                      labels: analysis.serviceRequirements!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.serviceRequirements!.monthlyBreakdown!
-                            .map((e) => e.requirementsCreated ?? 0)
-                            .toList(),
-                        analysis.serviceRequirements!.monthlyBreakdown!
-                            .map((e) => e.pendingRequirements ?? 0)
-                            .toList(),
-                        analysis.serviceRequirements!.monthlyBreakdown!
-                            .map((e) => e.fulfilledRequirements ?? 0)
-                            .toList(),
-                        analysis.serviceRequirements!.monthlyBreakdown!
-                            .map((e) => e.cancelledRequirements ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, Colors.orange, MyColors.green, Colors.red],
-                      legends: const [
-                        "Requirements Created",
-                        "Pending Requirements",
-                        "Fulfilled Requirements",
-                        "Cancelled Requirements",
-                      ],
-                    ),
-                  const Gap(10),
-                  if (analysis.productRequirements != null)
-                    ReportGraph(
-                      title: "Product Requirements Analysis",
-                      labels: analysis.productRequirements!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.productRequirements!.monthlyBreakdown!
-                            .map((e) => e.requirementsCreated ?? 0)
-                            .toList(),
-                        analysis.productRequirements!.monthlyBreakdown!
-                            .map((e) => e.pendingRequirements ?? 0)
-                            .toList(),
-                        analysis.productRequirements!.monthlyBreakdown!
-                            .map((e) => e.fulfilledRequirements ?? 0)
-                            .toList(),
-                        analysis.productRequirements!.monthlyBreakdown!
-                            .map((e) => e.cancelledRequirements ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [MyColors.primary, Colors.orange, MyColors.green, Colors.red],
-                      legends: const [
-                        "Requirements Created",
-                        "Pending Requirements",
-                        "Fulfilled Requirements",
-                        "Cancelled Requirements",
-                      ],
-                    ),
-                  const Gap(10),
-                  if (analysis.connectorSupportTickets != null)
-                    ReportGraph(
-                      title: "Support Ticket Analysis",
-                      labels: analysis.connectorSupportTickets!.monthlyBreakdown!
-                          .map((e) => e.monthName!)
-                          .toList(),
-                      values: [
-                        analysis.connectorSupportTickets!.monthlyBreakdown!
-                            .map((e) => e.openTickets ?? 0)
-                            .toList(),
-                        analysis.connectorSupportTickets!.monthlyBreakdown!
-                            .map((e) => e.closedTickets ?? 0)
-                            .toList(),
-                        analysis.connectorSupportTickets!.monthlyBreakdown!
-                            .map((e) => e.resolvedTickets ?? 0)
-                            .toList(),
-                      ],
-                      colors: const [Colors.orange, Colors.red, MyColors.green],
-                      legends: const ["Open Tickets", "Closed Tickets", "Resolved Tickets"],
-                    ),
-
-                  const Gap(20),
+                colors: const [MyColors.primary, MyColors.green],
+                legends: const ["Leads Created", "Converted to Sales"],
+              ),
+            const Gap(10),
+            if (analysis.salesLeadAnalytics != null &&
+                analysis.salesLeadAnalytics!.monthlyBreakdown != null)
+              ReportGraph(
+                title: "Sales Lead Analysis",
+                labels: analysis.salesLeadAnalytics!.monthlyBreakdown!
+                    .map((e) => e.monthName!)
+                    .toList(),
+                values: [
+                  analysis.salesLeadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.salesLeadsCreated ?? 0)
+                      .toList(),
+                  analysis.salesLeadAnalytics!.monthlyBreakdown!.map((e) => e.won ?? 0).toList(),
+                  analysis.salesLeadAnalytics!.monthlyBreakdown!.map((e) => e.lost ?? 0).toList(),
                 ],
-              );
+                colors: const [MyColors.primary, MyColors.green, Colors.red],
+                legends: const ["Sales Leads Created", "Won", "Lost"],
+              ),
+            const Gap(10),
+            if (analysis.accountLeadAnalytics != null &&
+                analysis.accountLeadAnalytics!.monthlyBreakdown != null)
+              ReportGraph(
+                title: "Account Lead Analysis",
+                labels: analysis.accountLeadAnalytics!.monthlyBreakdown!
+                    .map((e) => e.monthName!)
+                    .toList(),
+                values: [
+                  analysis.accountLeadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.accountLeadsCreated ?? 0)
+                      .toList(),
+                  analysis.accountLeadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.collected ?? 0)
+                      .toList(),
+                  analysis.accountLeadAnalytics!.monthlyBreakdown!
+                      .map((e) => e.pending ?? 0)
+                      .toList(),
+                ],
+                colors: const [MyColors.primary, MyColors.green, Colors.orange],
+                legends: const ["Account Leads Created", "Collected", "Pending"],
+              ),
+            const Gap(20),
+          ],
+        );
       }
       return const SizedBox();
     });
