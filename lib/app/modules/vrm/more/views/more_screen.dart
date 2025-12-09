abc94623facfd3ce8b6b6d7b6f3dd07d2a83c95a
Adhil ConstructionTechnect <adhil.constructiontechnect@gmail.com>
2025-12-09 11:47:46 +0530

    drafted more vrm screen

---
+++ lib/app/modules/vrm/more/views/more_screen.dart
@@ -0,0 +1,112 @@
+import 'package:construction_technect/app/core/utils/common_appbar.dart';
+import 'package:construction_technect/app/core/utils/imports.dart';
+import 'package:construction_technect/app/modules/MarketPlace/Partner/More/menu/menu_view.dart';
+import 'package:construction_technect/app/modules/vrm/more/controller/more_controller.dart';
+
+class VrmMoreScreen extends GetView<VrmMoreController> {
+  const VrmMoreScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MyColors.white,
+      appBar: const CommonAppBar(
+        isCenter: false,
+        leading: SizedBox(),
+        leadingWidth: 0,
+        title: Text("More"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Gap(1.h),
+              CommonContainer(
+                icon: Asset.person,
+                title: "My Account",
+                onTap: () {
+                  // if (myPref.role.val == "connector".toLowerCase()) {
+                  //   Get.toNamed(Routes.CONNECTOR_PROFILE);
+                  // } else {
+                  //   Get.toNamed(Routes.PROFILE);
+                  // }
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.cs,
+                title: "Customer support",
+                onTap: () {
+                  // Get.to(() => CustomerSupportView());
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.tutorial,
+                title: "Tutorials",
+                onTap: () {
+                  // openUrl(url: Constants.tutorial);
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.notification,
+                title: "Notification",
+                onTap: () {
+                  // Get.toNamed(Routes.NOTIFICATIONS);
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.location,
+                title: "Delivery location",
+                onTap: () {
+                  // if (myPref.role.val == "partner") {
+                  //   Get.toNamed(Routes.MANUFACTURER_ADDRESS);
+                  // } else {
+                  //   Get.toNamed(Routes.DELIVERY_LOCATION);
+                  // }
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.faq,
+                title: "F.A.Q’S",
+                onTap: () {
+                  // Get.toNamed(Routes.FAQ);
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.privacy,
+                title: "Privacy Policy",
+                onTap: () {
+                  // openUrl(url: Constants.privacyPolicy);
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.tc,
+                title: "Terms & conditions",
+                onTap: () {
+                  // openUrl(url: Constants.termsCondition);
+                },
+              ),
+              const Gap(16),
+              CommonContainer(
+                icon: Asset.message,
+                title: "Feedback",
+                onTap: () {
+                  // openUrl(url: Constants.aboutUS);
+                },
+              ),
+              const Gap(16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
